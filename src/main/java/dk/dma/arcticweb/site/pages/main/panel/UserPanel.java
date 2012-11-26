86b70ade11b3c7ecd08f9584829dc6178cec697b
Ole Bakman Borup <obo@dma.dk>
2012-11-26 15:14:49 +0100

    Finished user panel

--- src/main/java/dk/dma/arcticweb/site/pages/main/panel/UserPanel.java
+++ src/main/java/dk/dma/arcticweb/site/pages/main/panel/UserPanel.java
@@ -15,28 +15,21 @@ public class UserPanel extends Panel {
 	private static final long serialVersionUID = 1L;
 	
 	private static final Logger LOG = Logger.getLogger(UserPanel.class);
+	
+	private WebMarkupContainer notLoggedIn;
+	private WebMarkupContainer loggedIn;
 
 	public UserPanel(String id) {
 		super(id);
-				
-		WebMarkupContainer notLoggedIn = new WebMarkupContainer("not_logged_in") {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public boolean isVisible() {
-				return !ArcticWebSession.get().isLoggedIn();
-			}
-		};
-		add(notLoggedIn);
-		WebMarkupContainer loggedIn = new WebMarkupContainer("logged_in") {
-			private static final long serialVersionUID = 1L;
-			@Override
-			public boolean isVisible() {
-				return ArcticWebSession.get().isLoggedIn();
-			}
-		};
-		add(loggedIn);
 		
+		notLoggedIn = new WebMarkupContainer("not_logged_in");
+		loggedIn = new WebMarkupContainer("logged_in");
+		notLoggedIn.setVisible(false);
+		loggedIn.setVisible(false);
+				
+		add(notLoggedIn);
 		loggedIn.add(new Label("username", new PropertyModel<ArcticWebSession>(this, "username")));
+		add(loggedIn);
 		
 		Link<String> logoutLink = new Link<String>("logout_link") {
 			private static final long serialVersionUID = 1L;
@@ -47,8 +40,14 @@ public class UserPanel extends Panel {
 				setResponsePage(FrontPage.class);
 			}
 		};
-		loggedIn.add(logoutLink);
-		
+		loggedIn.add(logoutLink);		
+	}
+	
+	@Override
+	protected void onConfigure() {		
+		super.onConfigure();
+		loggedIn.setVisible(ArcticWebSession.get().isLoggedIn());
+		notLoggedIn.setVisible(!ArcticWebSession.get().isLoggedIn());
 	}
 	
 	public String getUsername() {		
