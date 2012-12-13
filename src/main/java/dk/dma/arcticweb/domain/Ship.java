b173fbb71166bd7d23b8479f0e7fefaebdac75ea
Ole Bakman Borup <obo@dma.dk>
2012-12-13 15:36:22 +0100

    More ship information

--- src/main/java/dk/dma/arcticweb/domain/Ship.java
+++ src/main/java/dk/dma/arcticweb/domain/Ship.java
@@ -11,6 +11,18 @@ public class Ship extends Stakeholder {
 	private static final long serialVersionUID = 1L;
 	
 	private Long mmsi;
+	private Long imoNo;
+	private String callsign;
+	private String type;
+	private Integer maxSpeed;
+	private Integer tonnage;	
+	private String commCapabilities;
+	private Integer rescueCapacity;
+	private Integer width;
+	private Integer length;
+	private String iceClass;
+	private Boolean helipad;
+	
 	private ShipOwner owner;
 	
 	public Ship() {
@@ -35,4 +47,103 @@ public class Ship extends Stakeholder {
 		this.owner = owner;
 	}
 
+	@Column(nullable = true)
+	public Long getImoNo() {
+		return imoNo;
+	}
+
+	public void setImoNo(Long imoNo) {
+		this.imoNo = imoNo;
+	}
+
+	@Column(nullable = true, length = 32)
+	public String getCallsign() {
+		return callsign;
+	}
+
+	public void setCallsign(String callsign) {
+		this.callsign = callsign;
+	}
+	
+	@Column(nullable = true, length = 32)
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Column(nullable = true)
+	public Integer getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(Integer maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	@Column(nullable = true)
+	public Integer getTonnage() {
+		return tonnage;
+	}
+
+	public void setTonnage(Integer tonnage) {
+		this.tonnage = tonnage;
+	}
+	
+	@Column(nullable = true, length = 32)
+	public String getCommCapabilities() {
+		return commCapabilities;
+	}
+
+	public void setCommCapabilities(String commCapabilities) {
+		this.commCapabilities = commCapabilities;
+	}
+
+	@Column(nullable = true)
+	public Integer getRescueCapacity() {
+		return rescueCapacity;
+	}
+
+	public void setRescueCapacity(Integer rescueCapacity) {
+		this.rescueCapacity = rescueCapacity;
+	}
+
+	@Column(nullable = true)
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
+	@Column(nullable = true)
+	public Integer getLength() {
+		return length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+
+	@Column(nullable = true, length = 32)
+	public String getIceClass() {
+		return iceClass;
+	}
+
+	public void setIceClass(String iceClass) {
+		this.iceClass = iceClass;
+	}
+
+	@Column(nullable = true)
+	public Boolean getHelipad() {
+		return helipad;
+	}
+
+	public void setHelipad(Boolean helipad) {
+		this.helipad = helipad;
+	}
+	
 }
