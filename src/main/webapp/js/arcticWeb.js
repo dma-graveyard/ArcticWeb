4de6a707e0f5e742231eb026633b91fcce8e6238
Niels Justesen <njustesen@gmail.com>
2012-12-19 15:43:05 +0100

    zoom to ship bug fixed

--- src/main/webapp/js/arcticWeb.js
+++ src/main/webapp/js/arcticWeb.js
@@ -1,13 +1,9 @@
 var my_ship;
 
 $(document).ready(function() {
-
+	
 	// Search after own ship based on ship_mmsi
 	$.getJSON(searchUrl, { argument: ship_mmsi }, function (result) {
-			var s = "s";
-			
-			// Show search results
-			$("#searchResults").css('visibility', 'visible');
 				
 			// Search results
 			searchResults = [];
@@ -24,7 +20,6 @@ $(document).ready(function() {
 				if (searchResults.length == 1){
 					my_ship = searchResults[0];
 				}
-				
 			}
 			
 			markedVessel = my_ship;
@@ -50,37 +45,15 @@ function loadSelectedInformation(){
 	
 	// Search after own ship based on ship_mmsi
 	$.getJSON(searchUrl, { argument: selectedVessel.mmsi }, function (result) {
-			var s = "s";
 			
-			// Show search results
-			$("#searchResults").css('visibility', 'visible');
-				
-			// Search results
-			searchResults = [];
-
-			// Get vessels
-			for (vesselId in result.vessels) {
-				var vesselJSON = result.vessels[vesselId];
-				var vessel = new Vessel(vesselId, vesselJSON, 1);
-				searchResults.push(vessel);
-			}
-
-			// Add search result to list
-			if (searchResults.length <= searchResultsLimit && searchResults.length != 0){
-				if (searchResults.length == 1){
-					searchedVessel = searchResults[0];
-				}
-				
-			}
-			
-			insertSelectedInformation(searchedVessel);
+			insertSelectedInformation(result);
 
 		});
 	
 }
 
-function insertSelectedInformation(vessel){
+function insertSelectedInformation(vesselInfo){
 	
-	alert(vessel);
+	alert(vesselInfo);
 	
 }
